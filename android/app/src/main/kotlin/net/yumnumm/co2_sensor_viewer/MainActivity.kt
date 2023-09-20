731c7a88d2ddca5a9bae51462faed4615c124caf
YumNumm <onory0917@gmail.com>
2023-09-21 01:44:54 +0900

    init

---
+++ android/app/src/main/kotlin/net/yumnumm/co2_sensor_viewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.yumnumm.co2_sensor_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:co2_sensor_viewer/app.dart';
+import 'package:flutter/widgets.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
