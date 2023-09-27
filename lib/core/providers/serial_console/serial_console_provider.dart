f65311c7c9253526619d8dfac283e959bd425180
YumNumm <onory0917@gmail.com>
2023-09-27 18:13:18 +0900

    fix: import path

--- lib/core/providers/serial_console/serial_console_provider.dart
+++ lib/core/providers/serial_console/serial_console_provider.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
+import 'package:co2_sensor_viewer/core/providers/serial_console/base/android_serial_console_impl.dart';
 import 'package:co2_sensor_viewer/core/providers/serial_console/base/others_serial_console_impl.dart';
 import 'package:co2_sensor_viewer/core/providers/serial_console/base/serial_console_base.dart';
-import 'package:co2_sensor_viewer/core/util/android_serial_console_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'serial_console_provider.g.dart';
